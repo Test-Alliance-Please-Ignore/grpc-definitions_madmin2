b891ca4ea88649e5c3fbc47cb983c36ab867f45a
Ozzeh <ozzeh@pleaseignore.com>
2019-09-16 16:57:45 -0500

    0.3 fuck you grpclib

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python -m grpc_tools.protoc -I proto --python_out=./ --grpc_python_out=./ --python_grpc_out=./ proto/grpc_madmin/.proto*
+python -m grpc_tools.protoc -I proto --python_out=./ --grpc_python_out=./ proto/grpc_madmin/*.proto
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="grpc_madmin",
-    version="0.2",
+    version="0.3",
     packages=["grpc_madmin"],
     install_requires=["grpcio", "googleapis-common-protos"],
     zip_safe=False,
