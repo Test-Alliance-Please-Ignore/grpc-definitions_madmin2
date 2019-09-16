c07560c18a4850aefd02b3e57edaed99959db162
Ozzeh <ozzeh@pleaseignore.com>
2019-09-16 16:40:37 -0500

    0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="grpc_madmin",
-    version="0.1",
+    version="0.2",
     packages=["grpc_madmin"],
     install_requires=["grpcio", "googleapis-common-protos"],
     zip_safe=False,
