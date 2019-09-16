6c4a05e7814ca0c2c79986ecb5e2a1d6c910b726
Ozzeh <ozzeh@pleaseignore.com>
2019-09-16 10:05:12 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="grpc_madmin",
+    version="0.1",
+    packages=["grpc_madmin"],
+    install_requires=["grpcio", "googleapis-common-protos"],
+    zip_safe=False,
+)
