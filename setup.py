d80c904c828dd50f31ac95c34cf59a49e420fa96
Ozzeh <ozzeh@pleaseignore.com>
2019-09-19 09:21:39 -0500

    0.5 actually, add InviteService

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="grpc_madmin",
-    version="0.4",
+    version="0.5",
     packages=["grpc_madmin"],
     install_requires=["grpcio", "googleapis-common-protos"],
     zip_safe=False,
