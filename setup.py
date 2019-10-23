5faaf291207548ca5a48cde3070dc214816880eb
ozzeh <ozzeh@pleaseignore.com>
2019-10-23 12:32:57 -0500

    0.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="grpc_madmin",
-    version="0.7",
+    version="0.9",
     packages=["grpc_madmin"],
     install_requires=["grpcio", "googleapis-common-protos"],
     zip_safe=False,
